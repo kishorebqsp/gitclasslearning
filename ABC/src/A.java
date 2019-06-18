8a7a4958ca5c4934c31935a81be37e46f7b1b9aa
Bharani <bharanigramesh28@gmail.com>
2019-06-18 18:03:43 +0530

    HI

---
+++ ABC/src/A.java
@@ -0,0 +1,13 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+public void abc()
+{
+System.out.println("Hello");
+System.out.println("Hello");
+
+	}
+
+}
